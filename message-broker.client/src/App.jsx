a462a68b5cfc092dcf29696d094988dd0f197a9c
Evhesha <evgenmedvedskii@gmail.com>
2025-03-02 17:02:32 +0300

    CLIENT: Add react-bootstrap extension.

--- message-broker.client/src/App.jsx
+++ message-broker.client/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div className="App">
-     
+      <Button variant="primary">
+          Hello Kirill
+        </Button>
     </div>
   );
 }
