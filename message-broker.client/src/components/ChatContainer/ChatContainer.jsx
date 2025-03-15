48ef8353e60bbd4aecbe94ff3619c6c775754977
noemo2004 <kirillz.2004@mail.ru>
2025-03-15 22:24:54 +0300

    Add new features

--- message-broker.client/src/components/ChatContainer/ChatContainer.jsx
+++ message-broker.client/src/components/ChatContainer/ChatContainer.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ChatContainer.css';
 
-
 const ChatContainer = ({ messages, sendMessage }) => {
+    const [showWelcomeMessage, setShowWelcomeMessage] = useState(true);
+    
+    const welcomeMessage = "👋 Привет! Я ваш личный чат-бот. Здесь, чтобы помочь вам с любыми вопросами. Просто напишите, и я постараюсь ответить! 😊";
+
     const handleKeyPress = (event) => {
         if (event.key === 'Enter') {
             sendMessage();
+            if (showWelcomeMessage) {
+                setShowWelcomeMessage(false);
+            }
         }
     };
+
+    const handleSendMessage = () => {
+        sendMessage();
+        if (showWelcomeMessage) {
+            setShowWelcomeMessage(false);
+        }
+    };
+
+    useEffect(() => {
+        if (messages.length > 0) {
+            setShowWelcomeMessage(false);
+        }
+    }, [messages]);
 
     return (
         <div className="chat-container">
             <div className="messages" id="messages">
+                {showWelcomeMessage && (
+                    <div className="welcome-message">
+                        {welcomeMessage}
+                    </div>
+                )}
                 {messages.map((msg, index) => (
                     <div key={index} className={`message ${msg.type}`}>
                         {msg.text}
@@ -26,8 +50,9 @@ const ChatContainer = ({ messages, sendMessage }) => {
                     autoFocus
                     onKeyPress={handleKeyPress}
                 />
-
-                <button class='button' onClick={sendMessage}>Отправить</button>
+                <button className="button" onClick={handleSendMessage}>
+                    Отправить
+                </button>
             </div>
         </div>
     );
