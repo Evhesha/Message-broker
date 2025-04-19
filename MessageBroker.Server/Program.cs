9a094c4548b1020ee4998ab58c8219c48e0cc375
Evhesha <evgenmedvedskii@gmail.com>
2025-04-19 15:13:59 +0300

    Can send messages to kafka from server project in json body, that let to send messages from client to server.

--- MessageBroker.Server/Program.cs
+++ MessageBroker.Server/Program.cs
@@ -1,25 +1,16 @@
-using MessageBroker.Kafka.Producer.Abstractions;
 using MessageBroker.Kafka.Producer.Extensions;
 using MessageBroker.Server.Extensions;
-using Microsoft.Extensions.AI;
-using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddCors();
 builder.Services.AddOpenApi();
-
 builder.Services.AddCustomCors(builder.Configuration);
 builder.Services.AddDataBase(builder.Configuration);
-
 builder.Services.AddKafkaProducer<string>(builder.Configuration.GetSection("Kafka:Question"));
 
 var app = builder.Build();
-
-Console.Write("Write your question --> ");
-string question = Console.ReadLine();
 
 if (app.Environment.IsDevelopment())
 {
@@ -27,19 +18,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapPost("/create-ollama-question", async (HttpContext httpContext) =>
-{
-    var kafkaProducer = httpContext.RequestServices.GetRequiredService<IKafkaProducer<string>>();
-
-    Console.WriteLine("Requesting a message to Kafka from server...");
-    await kafkaProducer.ProduceAsync(question, default);
-    Console.WriteLine("Message sent to Kafka!");
-});
-
+app.MapControllers(); 
 
 app.Run();
