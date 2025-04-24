5889ddb04199eb1079caf7e757880c6abccd9c70
noemo2004 <kirillz.2004@mail.ru>
2025-04-24 07:19:36 +0300

    Login/Registration page

--- message-broker.client/src/components/Sidebar/Sidebar.jsx
+++ message-broker.client/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Sidebar.css';
 import { Sun, Moon, Globe, BoxArrowInRight } from 'react-bootstrap-icons';
+import LoginModal from '../LoginModal/LoginModal';
 
 const Sidebar = ({ toggleTheme, startNewChat, chatHistory, isDarkTheme, toggleLanguage }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const toggleModal = () => setIsModalOpen(!isModalOpen);
+
     return (
         <div className={`sidebar ${isDarkTheme ? 'dark' : ''}`}>
             <div className="sidebar-header">
@@ -13,7 +18,7 @@ const Sidebar = ({ toggleTheme, startNewChat, chatHistory, isDarkTheme, toggleLa
                 >
                     {isDarkTheme ? <Moon size={18} /> : <Sun size={18} />}
                 </button>
-                
+
                 <button 
                     className="btn language-toggle"
                     onClick={toggleLanguage}
@@ -21,23 +26,25 @@ const Sidebar = ({ toggleTheme, startNewChat, chatHistory, isDarkTheme, toggleLa
                 >
                     <Globe size={18} />
                 </button>
-                
-                <button className="login-button">
+
+                <button className="login-button" onClick={toggleModal}>
                     <span className="login-text">Вход</span>
                     <BoxArrowInRight size={18} className="login-icon" />
                 </button>
             </div>
-            
+
             <button className="new-chat" onClick={startNewChat}>
                 Новый чат
             </button>
-            
+
             <h3>История сообщений</h3>
             <ul className="chat-list">
                 {chatHistory.map((chat, index) => (
                     <li key={index} className="chat-title">{chat.title}</li>
                 ))}
             </ul>
+
+            {isModalOpen && <LoginModal onClose={toggleModal} isDarkTheme={isDarkTheme} />}
         </div>
     );
 };
