47e2238b57e9bb37bac2f756e512d811ac7b73a9
Evhesha <evgenmedvedskii@gmail.com>
2025-05-11 17:10:47 +0300

    New repository changes, message and chat repositories were updated

--- MessageBroker.Server/Models/Chat.cs
+++ MessageBroker.Server/Models/Chat.cs
@@ -6,6 +6,9 @@ public class Chat
 {
     [BsonId]
     public string Id { get; set; }
+    
+    [BsonElement("userId")]
+    public string UserId { get; set; }
 
     [BsonElement("messages")]
     public List<Message> Messages { get; set; }
--- MessageBroker.Server/MongoDataAccess/Repositories/ChatsRepository.cs
+++ MessageBroker.Server/MongoDataAccess/Repositories/ChatsRepository.cs
@@ -1,13 +1,33 @@
-﻿namespace MessageBroker.Server.MongoDataAccess;
+﻿using MessageBroker.Server.Models;
+using MongoDB.Driver;
 
-public class ChatsRepository
+namespace MessageBroker.Server.MongoDataAccess;
+
+public class ChatRepository
 {
-    private readonly MongoDbContext _context;
+    private readonly IMongoCollection<Chat> _chatCollection;
 
-    public ChatsRepository(MongoDbContext context)
+    public ChatRepository(IMongoClient mongoClient)
     {
-        _context = context;
+        var database = mongoClient.GetDatabase("ChatDatabase");
+        _chatCollection = database.GetCollection<Chat>("Chats");
+    }
+
+    public async Task<Chat> GetChatByUserIdAsync(string userId)
+    {
+        var filter = Builders<Chat>.Filter.Eq("userId", userId);
+        return await _chatCollection.Find(filter).FirstOrDefaultAsync();
     }
     
+    public async Task CreateChatAsync(Chat chat)
+    {
+        await _chatCollection.InsertOneAsync(chat);
+    }
     
+    public async Task AddMessageToChatAsync(string chatId, Message message)
+    {
+        var filter = Builders<Chat>.Filter.Eq("_id", chatId);
+        var update = Builders<Chat>.Update.Push("messages", message);
+        await _chatCollection.UpdateOneAsync(filter, update);
+    }
 }
