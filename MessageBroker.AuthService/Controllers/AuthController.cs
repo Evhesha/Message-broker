b3a8a80b7bc9bda1482e0570ffb0d6732d4701a7
Evhesha <evgenmedvedskii@gmail.com>
2025-05-15 17:03:50 +0300

    Sql libraries and nuget packages were added to project. Entities and extensions.

---
+++ MessageBroker.AuthService/Controllers/AuthController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MessageBroker.AuthService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    
+}
