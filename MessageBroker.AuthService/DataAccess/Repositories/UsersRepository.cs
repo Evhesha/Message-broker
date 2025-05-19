f8e8748cf5af42039be8476b1956a2b2ae0fe33d
Evhesha <evgenmedvedskii@gmail.com>
2025-05-19 18:42:08 +0300

    Auth service changes and features were added

---
+++ MessageBroker.AuthService/DataAccess/Repositories/UsersRepository.cs
@@ -0,0 +1,20 @@
+﻿using MessageBroker.AuthService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessageBroker.AuthService.DataAccess.Repositories;
+
+public class UsersRepository
+{
+    ApplicationDbContext _context;
+    
+    public UsersRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public UserEntity? GetUser(string email)
+    {
+        return _context.Users.
+            FirstOrDefault(u => u.Email == email);
+    }
+}
--- MessageBroker.AuthService/Entities/UserEntity.cs
+++ MessageBroker.AuthService/Entities/UserEntity.cs
@@ -2,5 +2,7 @@
 
 public class UserEntity
 {
-    
+    public Guid Id { get; set; }
+    public string HashedPassword { get; set; }
+    public string Email { get; set; }
 }
